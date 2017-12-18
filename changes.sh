d0662e9adb5dd17d3011af8c3c51b18caad07b31
Marcel Körtgen <marcel.koertgen@gmail.com>
2017-12-18 14:46:45 +0100

    Match top-level directory names only, Fix #1

--- changes.sh
+++ changes.sh
@@ -6,7 +6,7 @@ ref=${ref:-HEAD~}
 
 echo "Checking for changes of folder '${folder}' from ref '${ref}'..."
 
-git diff ${ref} --name-only | grep -qw ${folder}
+git diff ${ref} --name-only | grep -qw "^${folder}"
 changes=$?
 if [[ ${changes} -eq 0 ]]; then
   echo "Folder '${folder}' has changed."
