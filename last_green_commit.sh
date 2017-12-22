8db25c19388a1821a7d285eda93ab2bd5e3b6aef
Marcel Körtgen <marcel.koertgen@gmail.com>
2017-12-22 14:28:27 +0100

    Fix quoting issue. Fix #2

--- last_green_commit.sh
+++ last_green_commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIRNAME="$(dirname $(readlink -f "$0"))"
 pushd ${DIRNAME}
-url=${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/pipelines?private_token=${PRIVATE_TOKEN}&status=success&ref=${CI_COMMIT_REF_NAME}
+url="${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/pipelines?private_token=${PRIVATE_TOKEN}&status=success&ref=${CI_COMMIT_REF_NAME}"
 commit=$(curl -s ${url} | jq -r -f jq.filter)
 echo "Last green commit is '${commit}'."
 echo ${commit} > .LAST_GREEN_COMMIT
