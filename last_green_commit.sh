138b304169aa1c2b043d3495a264b9ce325c99aa
Marcel Körtgen <marcel.koertgen@gmail.com>
2019-01-10 21:17:40 +0100

    .

--- last_green_commit.sh
+++ last_green_commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIRNAME="$(dirname $(readlink -f "$0"))"
 pushd ${DIRNAME}
-header="PRIVATE-TOKEN: ${PRIVATE_TOKEN:-${CI_JOB_TOKEN}}"
+header="PRIVATE-TOKEN: ${PRIVATE_TOKEN}"
 url="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/pipelines?status=success&ref=${CI_COMMIT_REF_NAME}"
 commit=$(curl -H ${header}  -s ${url} | jq -r -f jq.filter)
 echo "Last green commit is '${commit}'."
