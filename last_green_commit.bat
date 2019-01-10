0601b778b7a440d5c042b1f523d9742b2dae3889
Marcel Körtgen <marcel.koertgen@gmail.com>
2019-01-10 20:41:46 +0100

    fixes

--- last_green_commit.bat
+++ last_green_commit.bat
@@ -1,8 +1,9 @@
 @echo off
 setlocal
 pushd %~dp0
-set url="%CI_SERVER_URL%/api/v4/projects/%CI_PROJECT_ID%/pipelines?private_token=%PRIVATE_TOKEN%&ref=%CI_COMMIT_REF_NAME%&status=success"
-curl -s %url% | jq -r -f jq.filter > .LAST_GREEN_COMMIT
+set header="PRIVATE_TOKEN: %CI_JOB_TOKEN%"
+set url="%CI_SERVER_URL%/api/v4/projects/%CI_PROJECT_ID%/pipelines?status=success&ref=%CI_COMMIT_REF_NAME%"
+curl -H %header% -s %url% | jq -r -f jq.filter > .LAST_GREEN_COMMIT
 set /P commit=<.LAST_GREEN_COMMIT
 echo Last green commit is '%commit%'.
 popd
--- last_green_commit.sh
+++ last_green_commit.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 DIRNAME="$(dirname $(readlink -f "$0"))"
 pushd ${DIRNAME}
-url="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/pipelines&status=success&ref=${CI_COMMIT_REF_NAME}"
-commit=$(curl --header "PRIVATE-TOKEN: $CI_JOB_TOKEN" -s ${url} | jq -r -f jq.filter)
+header="PRIVATE-TOKEN: ${CI_JOB_TOKEN}"
+url="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/pipelines?status=success&ref=${CI_COMMIT_REF_NAME}"
+commit=$(curl -H ${header}  -s ${url} | jq -r -f jq.filter)
 echo "Last green commit is '${commit}'."
 echo ${commit} > .LAST_GREEN_COMMIT
 popd
