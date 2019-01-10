b8a0868b5dd79845859d1a7aa2b51d54bd7809b7
Marcel Körtgen <marcel.koertgen@gmail.com>
2019-01-10 18:22:49 +0100

    Try simplify token

--- last_green_commit.sh
+++ last_green_commit.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 DIRNAME="$(dirname $(readlink -f "$0"))"
 pushd ${DIRNAME}
-# https://gitlab.com/api/v4
-api_url=${CI_API_V4_URL:-${CI_SERVER_URL}/api/v4}
-token=${CI_JOB_TOKEN:-${PRIVATE_TOKEN}}
-url="${api_url}/projects/${CI_PROJECT_ID}/pipelines?private_token=${token}&status=success&ref=${CI_COMMIT_REF_NAME}"
-commit=$(curl -s ${url} | jq -r -f jq.filter)
+url="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/pipelines&status=success&ref=${CI_COMMIT_REF_NAME}"
+commit=$(curl --header "PRIVATE-TOKEN: $CI_JOB_TOKEN" -s ${url} | jq -r -f jq.filter)
 echo "Last green commit is '${commit}'."
 echo ${commit} > .LAST_GREEN_COMMIT
 popd
