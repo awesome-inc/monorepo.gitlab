1939bee8a7f0579f6ac8189b96535c6025883603
Marcel Körtgen <marcel.koertgen@gmail.com>
2019-01-10 20:47:00 +0100

    .

--- last_green_commit.bat
+++ last_green_commit.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 pushd %~dp0
-set header="PRIVATE_TOKEN: %CI_JOB_TOKEN%"
+set header="PRIVATE_TOKEN: %PRIVATE_TOKEN%"
 set url="%CI_SERVER_URL%/api/v4/projects/%CI_PROJECT_ID%/pipelines?status=success&ref=%CI_COMMIT_REF_NAME%"
 curl -H %header% -s %url% | jq -r -f jq.filter > .LAST_GREEN_COMMIT
 set /P commit=<.LAST_GREEN_COMMIT
