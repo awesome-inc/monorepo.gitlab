15b2a462b7336c1e20ede2c56da50f493b6cd670
Joshua Knarr <joshua.knarr@lexisnexisrisk.com>
2019-06-26 19:47:44 -0400

    slightly better behaved version

--- last_green_commit.sh
+++ last_green_commit.sh
@@ -3,7 +3,9 @@ DIRNAME="$(dirname $(readlink -f "$0"))"
 pushd ${DIRNAME}
 header="PRIVATE-TOKEN: ${PRIVATE_TOKEN}"
 url="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/pipelines?status=success&ref=${CI_COMMIT_REF_NAME}"
-commit=$(curl --header "${header}"  -s "${url}" | jq -r -f jq.filter)
-echo "Last green commit is '${commit}'."
-echo ${commit} > .LAST_GREEN_COMMIT
+curl --header "${header}"  -s "${url}" -o tmp
+jq -r -f jq.filter < tmp > .LAST_GREEN_COMMIT
+echo "Last green commit is"
+cat .LAST_GREEN_COMMIT
+rm tmp
 popd
